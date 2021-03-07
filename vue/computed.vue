f2831dffc2513e56864b59e3c30800b4f90b9cca
“SeiyaHirata” <s-hirata@g-account.page>
2021-03-07 14:43:01 +0900

    computed例

---
+++ vue/computed.vue
@@ -0,0 +1,21 @@
+
+<script>
+// computed
+export default {
+  data() {
+    return {
+      price: 100,
+    };
+  },
+  computed: {
+    doublePrice: {
+      get() {
+        return this.price * 2;
+      },
+      set(value) {
+        this.price = value / 2;
+      },
+    },
+  },
+};
+</script>
